dad6502cd4bad12325629c2a8830cc88bc4496cc
Artem Konstantinov <don666vito@yandex.ua>
2017-09-15 14:09:25 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile("server.log", log + "\n", (err) => {
+        if (err) {
+            console.log("Unable to append to server.log");
+        }
+    });
+    next();
+});
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase();
+});
+
+app.get("/", (req, res) => {
+    res.render("home.hbs", {
+        pageTitle: "Test Home Page",
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: "Hello Friend"
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.hbs", {
+        pageTitle: "About Page",
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: "Hello Friend this is About page"
+    });
+});
+
+app.get("/projects", (req, res) => {
+    res.render("projects.hbs", {
+        pageTitle: "Projects Page",
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: "Hello Friend this is Project page"
+    });
+});
+
+
+app.get("/bad", (req, res) => {
+    res.send({
+        error: "#qweqweq"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
